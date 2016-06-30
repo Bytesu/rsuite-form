66f2c316fcd7657d33217ab5eb98d1c0743f0af0
theJian <thejianmail@gmail.com>
2016-06-30 16:08:18 +0800

    init env

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const PATHS = {
+    src: path.join(__dirname, 'src'),
+    dist: path.join(__dirname, 'dist')
+};
+
+module.exports = {
+    devtool: 'eval',
+    entry: PATHS.src,
+    output: {
+        path: PATHS.dist,
+        filename: 'bundle.js'
+    },
+    devServer: {
+        hot: true,
+        inline: true,
+        contentBase: PATHS.dist
+    },
+    module: {
+        loaders: [
+        {
+            test: /\.jsx?$/,
+            loaders: 'babel',
+            query: {
+                cacheDirectory: true,
+                presets: ['es2015', 'stage-0', 'react']
+            },
+            include: PATHS.src
+        }
+        ]
+    }
+}
