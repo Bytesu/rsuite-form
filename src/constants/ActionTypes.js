5dce6b84578a097f85135a94e3b96bf61edfcef9
theJian <thejianmail@gmail.com>
2016-07-04 10:47:55 +0800

    rename ActoinTypes -> actionTypes

--- src/constants/ActionTypes.js
+++
@@ -1,2 +0,0 @@
-export const SUBMIT_FORM = 'SUBMIT_FORM';
-export const CHANGE_FORM_VALUE = 'CHANGE_FORM_VALUE';
---
+++ src/constants/actionTypes.js
@@ -0,0 +1,2 @@
+export const SUBMIT_FORM = 'SUBMIT_FORM';
+export const CHANGE_FIELD_VALUE = 'CHANGE_FIELD_VALUE';
