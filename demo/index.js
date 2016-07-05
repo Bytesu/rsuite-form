d8d1dec83410a70864f4d5c1fb5f28b64044b151
theJian <thejianmail@gmail.com>
2016-07-05 15:09:11 +0800

    schema demo

--- demo/index.js
+++ demo/index.js
@@ -4,10 +4,31 @@ import Form from '../src/index.js';
 
 class FarmerJohn extends React.Component {
     render() {
+        const schema = {
+            username: {
+                          validators: [
+                              // username should at least 6 characters long and contain only letter
+                              Form.validators.isString,
+                              Form.validators.isLongerThan(6),
+                              Form.validators.containsLetterOnly
+                          ]
+                      },
+            passwd: {
+                          validators: [
+                              // password should at least 6 characters long and contain number, uppercase letter and lowercase letter
+                              Form.validators.isString,
+                              Form.validators.isLongerThan(6),
+                              Form.validators.containsNumber,
+                              Form.validators.containsLowercaseLetter,
+                              Form.validators.containsUppercaseLetter
+                          ]
+                      }
+        };
+
         return (
-            <Form.Form onSubmit={(formData) => console.log(formData)}>
-                <Form.Field name="username" type="PlainText"/>
-                <Form.Field name="passwd" type="Password"/>
+            <Form.Form schema={schema} onSubmit={(formData) => console.log(formData)}>
+                <Form.Field name="username" type="PlainText" helpText="username helptext"/>
+                <Form.Field name="passwd" type="Password" helpText="password helptext"/>
                 <Form.SubmitButton text="click to submit" />
             </Form.Form>
         );
