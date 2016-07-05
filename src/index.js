637a7d0aa52470846c58d11e941dea34544f3c7b
theJian <thejianmail@gmail.com>
2016-07-05 15:08:05 +0800

    export validators

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import reducer from './reducers';
 import Form from './containers/Form.js';
 import Field from './components/Field.js';
+import * as validators from './utils/validators.js';
 import SubmitButton from './components/SubmitButton.js';
 
 const store = createStore(reducer);
@@ -11,5 +12,6 @@ const store = createStore(reducer);
 export default {
     Form: (props) => <Provider store={store}><Form {...props} /></Provider>,
     Field,
-    SubmitButton
+    SubmitButton,
+    validators
 };
