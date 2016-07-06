086d65082666b0d08442d4b65c432c4f2e485e19
theJian <thejianmail@gmail.com>
2016-07-06 18:46:37 +0800

    update demo for new Schema

--- demo/index.js
+++ demo/index.js
@@ -1,35 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Form from '../src/index.js';
+import { SchemaBuilder, StringType } from '../src/utils/Schema';
 
 class FarmerJohn extends React.Component {
     render() {
-        const schema = {
-            username: {
-                          validators: [
-                              // username should at least 6 characters long and contain only letter
-                              Form.validators.isString,
-                              Form.validators.isLongerThan(6),
-                              Form.validators.containsLetterOnly
-                          ]
-                      },
-            passwd:   {
-                          validators: [
-                              // password should at least 6 characters long and contain number, uppercase letter and lowercase letter
-                              Form.validators.isString,
-                              Form.validators.isLongerThan(6),
-                              Form.validators.containsNumber,
-                              Form.validators.containsLowercaseLetter,
-                              Form.validators.containsUppercaseLetter
-                          ]
-                      },
-            bio:      {
-                          validators: [
-                              Form.validators.isString
-                          ]
-                      }
-        };
-
+        const schema = SchemaBuilder({
+            username: StringType().isLongerThan(6)
+                                  .containsLetterOnly(),
+            passwd:   StringType().isLongerThan(6)
+                                  .containsNumber()
+                                  .containsLowercaseLetter()
+                                  .containsUppercaseLetter(),
+            bio:      StringType()
+        });
         return (
             <Form.Form schema={schema} onSubmit={(formData) => console.log(formData)}>
                 <Form.Field name="username" type="PlainText" helpText="username helptext"/>
@@ -45,4 +29,3 @@ render(
     <FarmerJohn />,
     document.getElementById('render-target')
 );
-
