31885640fd0b5436d0d5139cc22eaa4459a93a61
theJian <thejianmail@gmail.com>
2016-07-08 17:59:59 +0800

    update demo for new features

--- demo/index.js
+++ demo/index.js
@@ -4,6 +4,30 @@ import Form from '../src/index.js';
 import { SchemaBuilder, StringType, NumberType } from '../src/utils/Schema';
 
 class FarmerJohn extends React.Component {
+    getGenderList() {
+        return [
+        { value: 0, text: 'male' },
+        { value: 1, text: 'female' },
+        { value: 2, text: 'both' },
+        { value: 3, text: 'neither' }
+        ];
+    }
+
+    getCountryList() {
+        return [
+        { value: '000', text: 'ch' },
+        { value: '001', text: 'tw' },
+        { value: '002', text: 'hk' },
+        { value: '003', text: 'jp' },
+        { value: '004', text: 'ko' },
+        { value: '005', text: 'us' },
+        { value: '006', text: 'fr' },
+        { value: '007', text: 'uk' },
+        { value: '008', text: 'ca' },
+        { value: '009', text: 'au' }
+        ];
+    }
+
     render() {
         const schema = SchemaBuilder({
             username: StringType('username is unvalid').isLongerThan(6, 'username must be longer than 6 characters')
@@ -13,25 +37,16 @@ class FarmerJohn extends React.Component {
                                                        .containsLowercaseLetter('password must contain lowercase letters')
                                                        .containsUppercaseLetter('password must contain uppercase letters'),
             bio:      StringType('bio is unvalid'),
-            gender:   NumberType('gender unvalid').isOneOf([...Array(4).keys()])
+            gender:   NumberType('gender unvalid').isOneOf([...Array(4).keys()]),
+            country:  StringType('country is unvalid').isOneOf(this.getCountryList().map(o => o.value))
         });
         return (
             <Form.Form schema={schema} onSubmit={(formData) => console.log(formData)}>
                 <Form.Field name="username" type="PlainText" />
                 <Form.Field name="passwd" type="Password" />
                 <Form.Field name="bio" type="TextArea" placeholder="textarea placeholder"/>
-                <Form.Field
-                    name="gender"
-                    type="Radios"
-                    options={
-                        [
-                        { value: 0, text: 'male' },
-                        { value: 1, text: 'female' },
-                        { value: 2, text: 'both' },
-                        { value: 3, text: 'neither' }
-                        ]
-                    }
-                />
+                <Form.Field name="gender" type="Radios" options={this.getGenderList()} />
+                <Form.Field name="country" type="DropDownList" options={this.getCountryList()} />
                 <Form.SubmitButton text="click to submit" />
             </Form.Form>
         );
