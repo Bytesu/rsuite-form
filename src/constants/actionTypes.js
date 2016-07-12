2533d4e0ec4b25b686d5aed2caf0b49f5a063196
theJian <thejianmail@gmail.com>
2016-07-12 18:11:24 +0800

    delete redux related files

--- src/constants/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const CHANGE_FIELD_VALUE = 'CHANGE_FIELD_VALUE';
-export const CLEAR = 'CLEAR';
--- src/containers/Form.js
+++
@@ -1,6 +0,0 @@
-import Form from '../components/Form.js';
-import { connect } from 'react-redux';
-
-export default connect(state => ({
-    formData: state.formData
-}))(Form);
