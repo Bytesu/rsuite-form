5e97198ea4a1f8da0b6f78bb095cd02bb798c975
theJian <thejianmail@gmail.com>
2016-07-12 10:45:15 +0800

    remove submit button & validators

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import { Provider } from 'react-redux';
 import reducer from './reducers';
 import Form from './containers/Form.js';
 import Field from './components/Field.js';
-import * as validators from './utils/validators.js';
-import SubmitButton from './components/SubmitButton.js';
 
 const store = createStore(reducer);
 
 export default {
     Form: (props) => <Provider store={store}><Form {...props} /></Provider>,
     Field,
-    SubmitButton,
-    validators
 };
