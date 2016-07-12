1b089cf313c4cd6a9213a525904fd667ccf23b2a
theJian <thejianmail@gmail.com>
2016-07-12 16:59:06 +0800

    update demo for new feature

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render, findDOMNode } from 'react-dom';
-import Form from '../src/index.js';
+import Rsf from '../src/index.js';
 import { SchemaBuilder, StringType, NumberType } from '../src/utils/Schema';
 
 class PlainText extends React.Component {
     static propTypes = {
-        onChange: React.PropTypes.func
+        onChange: React.PropTypes.func,
+        value: React.PropTypes.string
     };
 
     onInputValueChange() {
@@ -17,7 +18,7 @@ class PlainText extends React.Component {
 
     render() {
         return (
-            <input type="text" ref="input" onChange={this.onInputValueChange.bind(this)}/>
+            <input type="text" ref="input" onChange={this.onInputValueChange.bind(this)} value={this.props.value || ''}/>
         );
     }
 }
@@ -28,9 +29,13 @@ class FarmerJohn extends React.Component {
             data: StringType('input invalid')
         });
         return (
-            <Form.Form schema={schema} onSubmit={(formData) => console.log(formData)}>
-                <Form.Field name="data"> <PlainText onChange={() => console.log('custom onChange()')}/> </Form.Field>
-            </Form.Form>
+            <Rsf.Form schema={schema} ref="form">
+                <Rsf.Field name="data"> <PlainText onChange={() => console.log('custom onChange()')} /> </Rsf.Field>
+                <button onClick={() => console.log(this.refs.form.getData())}>submit</button>
+                <button onClick={() => {
+                    this.refs.form.reset();
+                }}>clear</button>
+            </Rsf.Form>
         );
     }
 }
