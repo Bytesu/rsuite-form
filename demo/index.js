3478ee5a86c7d319a77461815fca5f54ebb9586b
theJian <thejianmail@gmail.com>
2016-07-21 11:15:52 +0800

    update demo

--- demo/index.js
+++ demo/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { render, findDOMNode } from 'react-dom';
-import {Form, Field} from '../src/index.js';
-import { SchemaBuilder, StringType, NumberType } from '../src/utils/Schema';
-
-const userModel = {
-    username: 'farmer john',
-    email: 'john@farmer.cow'
-};
+import { Form, Field } from '../src/index.js';
+import { SchemaBuilder, StringType } from '../src/utils/Schema';
 
 class PlainText extends React.Component {
     static propTypes = {
         onChange: React.PropTypes.func,
-        value: React.PropTypes.string
+        isValid: React.PropTypes.bool,
+        errorMessage: React.PropTypes.string,
+        value: React.PropTypes.string,
+        force: React.PropTypes.bool
     };
 
     onInputValueChange() {
@@ -21,9 +19,26 @@ class PlainText extends React.Component {
         onChange && onChange(value);
     }
 
+    shouldShowHelpBlock() {
+        const { isValid, force, value } = this.props;
+        return force || !isValid && value !== undefined;
+    }
+
+    getHelpBlock() {
+        if(this.shouldShowHelpBlock()) {
+            return (
+                <span className="help-block"> { this.props.errorMessage } </span>
+            );
+        }
+    }
+
     render() {
+        const { isValid, errorMessage, value, force } = this.props;
         return (
-            <input type="text" ref="input" onChange={this.onInputValueChange.bind(this)} value={this.props.value || ''}/>
+            <div className="form-control">
+                <input type="text" ref="input" onChange={this.onInputValueChange.bind(this)} value={value || ''} />
+                {this.getHelpBlock()}
+            </div>
         );
     }
 }
@@ -36,7 +51,8 @@ class FarmerJohn extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: props.data || {...userModel}
+            data: props.data || {},
+            forceValidation: false
         };
     }
 
@@ -45,19 +61,27 @@ class FarmerJohn extends React.Component {
     }
 
     reset() {
-        this.setState({data: {...userModel}});
+        this.setState({data: {}});
+    }
+
+    handleSubmit() {
+        console.log(this.state.data);
+        this.setState({forceValidation: true});
     }
 
     render() {
         const schema = SchemaBuilder({
-            data: StringType('input invalid')
+            username: StringType('username required')
+                      .isLongerThan(6, 'username must longer than 6'),
+            email:    StringType('email required')
+                      .isValidEmail('no a valid email address')
         });
 
         return (
-            <Form schema={schema} formData={this.state.data} onChange={() => console.log('form changed')} ref="form">
-                <Field name="username"> <PlainText onChange={() => console.log('custom onChange()')} /> </Field>
-                <Field name="email"> <PlainText onChange={() => console.log('custom onChange()')} /> </Field>
-                <button onClick={() => console.log(this.getData())}>submit</button>
+            <Form schema={schema} formData={this.state.data} onChange={() => console.log('form changed')} force={this.state.forceValidation} ref="form">
+                <Field name="username"> <PlainText onChange={() => console.log('username changed')} /> </Field>
+                <Field name="email"> <PlainText onChange={() => console.log('email changed')} /> </Field>
+                <button onClick={this.handleSubmit.bind(this)}>submit</button>
                 <button onClick={this.reset.bind(this)}>reset</button>
             </Form>
         );
